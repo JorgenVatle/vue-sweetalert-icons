c8f5402986e4be3eecc112ac9dd2bb9e63a91819
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-09 00:58:03 +0300

    Implement vue-sfc-rollup's SFC packaging approach.

---
+++ src/entry.js
@@ -0,0 +1,25 @@
+import SweetalertIcon from './components/icon.vue';
+
+function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component('SweetalertIcon', SweetalertIcon);
+}
+
+const plugin = {
+    install,
+};
+
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+SweetalertIcon.install = install;
+
+export default SweetalertIcon;
