ead56cbc84e44cbd5bd5057d6701606567671b12
Jørgen Vatle <Jorgen@WcAServices.net>
2023-09-25 23:16:54 +0300

    Add plugin-vue to Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import Vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    plugins: [
+        Vue()
+    ],
     build: {
         lib: {
             entry: resolve(__dirname, './src/entry.js'),
